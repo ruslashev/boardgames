56a73dac9abb2796536523814f78dbe5afb1a3bc
ruslashev <ruslashev@gmail.com>
2014-06-01 13:34:29 +0400

    Improve board drawing, add modes

--- reversi/src/board.cpp
+++ reversi/src/board.cpp
@@ -2,21 +2,36 @@
 
 void DrawBoard()
 {
-	printf("   ");
-	for (int i = 0; i < 8; i++) {
-		putchar('a'+i);
-		putchar(' ');
-	}
-	putchar('\n');
-	for (int y = 0; y < 8; y++) {
-		putchar('1'+y);
-		putchar(' ');
-		for (int x = 0; x < 8; x++) {
-			putchar('|');
-			putchar(CellsForDrawing[Board[y][x]]);
+	const bool numbersOnTheRight = true;
+	
+	if (numbersOnTheRight) {
+		printf("  ");
+		for (int i = 0; i < 8; i++) {
+			printf("%c ", 'a'+i);
 		}
-		putchar('|');
 		putchar('\n');
+		for (int y = 0; y < 8; y++) {
+			putchar(' ');
+			for (int x = 0; x < 8; x++) {
+				putchar('|');
+				putchar(CellsForDrawing[Board[y][x]]);
+			}
+			printf("| %c\n", '1'+y);
+		}
+	} else {
+		printf("   ");
+		for (int i = 0; i < 8; i++) {
+			printf("%c ", 'a'+i);
+		}
+		putchar('\n');
+		for (int y = 0; y < 8; y++) {
+			printf("%c ", '1'+y);
+			for (int x = 0; x < 8; x++) {
+				putchar('|');
+				putchar(CellsForDrawing[Board[y][x]]);
+			}
+			printf("|\n");
+		}
 	}
 }
 
